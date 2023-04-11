a4fed74db9efac7bd78cab1a53c6822f43d9d494
James Brundage <@github.com>
2023-04-11 13:22:58 -0700

    Adding GitLogger Action (Fixes #1)

---
+++ GitLogger.GitHubAction.PSDevOps.ps1
@@ -0,0 +1,8 @@
+﻿#requires -Module PSDevOps
+#requires -Module GitLoggerAction
+Import-BuildStep -ModuleName GitLoggerAction
+Push-Location $PSScriptRoot
+New-GitHubAction -Name "LogGit" -Description @'
+Logs to GitLogger
+'@ -Action GitLoggerAction -Icon git-commit -OutputPath .\action.yml
+Pop-Location
